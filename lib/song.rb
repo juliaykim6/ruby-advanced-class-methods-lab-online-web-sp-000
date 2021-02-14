00fa41607a43b3725ed1b864ac168da61a78667b
Julia Kim <juliaykim6@gmail.com>
2021-02-14 22:32:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,4 +40,14 @@ class Song
     self.all.sort_by{|song| song.name}
   end
 
+  def self.new_from_filename(filename)
+    song = filename.split(" - ")
+    artist_name = song[0]
+    song_name = song[1].gsub(".mp3", "")
+
+    song = self.new
+    song.name = song_name
+    song.artist = artist_name
+    song
+    song.save
 end
